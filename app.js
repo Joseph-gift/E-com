6a10a36233b91840b7d4cbc609761c9062f7c790
Joseph-gift <godonoujoseph.jg@gmail.com>
2025-02-14 09:05:33 +0000

    add Payment modal

--- app.js
+++ app.js
@@ -123,3 +123,17 @@ currentProductSizes.forEach((size, index) => {
     size.style.color = "white"
   })
 })
+
+// open and close payment method
+
+const productButton = document.querySelector(".productButton");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
+
+productButton.addEventListener("click",()=>{
+  payment.style.display="flex"
+})
+
+close.addEventListener("click",()=>{
+  payment.style.display="none"
+})
